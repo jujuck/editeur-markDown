fe378edbaf5bd4666f348d963a590f4a56a05572
jujuck <julien.y.richard@gmail.com>
2018-06-25 14:31:35 +0200

    tutoriel React-MarkDown

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+
+//css
+import './style/css/bootstrap.min.css';
+import './style/css/style.css';
+
+//js
+import {sampleText} from './style/js/sample';
+
+//Markdown
+import marked from 'marked';
+
+class App extends Component {
+  state = {
+    text: sampleText
+  };
+
+  componentWillMount() {
+    const localStorageText = localStorage.getItem('text');
+    if(localStorageText) {
+      this.setState({ text : localStorageText});
+    }
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    localStorage.setItem('text', nextState.text);
+  }
+
+  editText(event) {
+    const text = event.target.value;
+    this.setState({text : text})
+  }
+
+  renderText(text) {
+    const renderText = marked(text, {sanitize: true});
+    return { __html: renderText };
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6">
+            <textarea 
+              rows="35" 
+              className="form-control" 
+              value={this.state.text}
+              onChange= {(e) => this.editText(e)}
+              >
+            </textarea>
+          </div>
+          <div className="col-sm-6">
+            <div dangerouslySetInnerHTML={this.renderText(this.state.text)}></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
